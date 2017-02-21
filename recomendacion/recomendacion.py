ec1b4faf44a3e014c715a072e0617212b19ec7fb
Jose Miguel <jose.miguel.db.17894@gmail.com>
2017-02-21 16:14:18 +0100

    recomendacion de usuarios

--- recomendacion/recomendacion.py
+++ recomendacion/recomendacion.py
@@ -17,21 +17,23 @@ def diccionarioUsuario(pathToFile):
 				user.get(userID)[filmID]=rating
 
 	print (user)
-a = [5,3,4,4]
-b = [3,1,2,3,3]
-c = [4,3,4,3,5]
-d = [3,3,1,5]
-e = [1,5,5,2]
 
-ve = [b,c]
-
+#Define el ejemplo de la pagina 6
+def example():
+    a = [5,3,4,4]
+    b = [3,1,2,3,3]
+    c = [4,3,4,3,5]
+    d = [3,3,1,5,4]
+    e = [1,5,5,2,1]
+    conj = [a,b,c,d,e]
+    return conj
 
 # x: dataset
 # y: dataset
-
 def similarity(x,y):
 	r,_= pearsonr(x, y)
 	return (r)
+
 
 # x: dataset
 def media(x):
@@ -40,15 +42,49 @@ def media(x):
 		media +=x[i]
 	return (media / len (x))
 
+# x: dataset
+# y: dataset
+def equalItems(x,y):
+    lenx = len(x)
+    leny = len(y)
+    elements = abs(lenx - leny)
+    if(lenx > leny):
+        for i in range(elements):
+            x.pop()
+    elif(leny > lenx):
+        for i in range(elements):
+            y.pop()
+    return x,y
+
+#A partir del conjunto de usuarios y del usuario a comprobar la similaridad,
+# devuelve el conjunto de vecinos que supera el umbral de similaridad dado.
+def neighborhood(x,conj,threshold):
+    neighborhood = []
+    sim = 0
+    valx = x[:]
+    valy = []
+    for i in range(len(conj)):
+        valy = conj[i][:]
+        valx, valy = equalItems(valx,valy)
+        sim = similarity(valx,valy)
+        print("Similaridad:",sim)
+        if(sim >= threshold and valx != valy):
+            neighborhood.append(conj[i])
+    print(conj)
+    return neighborhood
 # a : dataset
 # p : object not in a
-# v : set of dataset
-def prediction (a ,p ,v):
+# N : set of dataset
+def prediction (a ,p ,N):
 	r = media(a)
-	for i in range (len (v)):
+	for i in range (len(N)):
 		aux = v.remove(p)	
 		print (similarity(a,aux))
 
 
-diccionarioUsuario("u.data")
-print (prediction(a,3,ve))
+#diccionarioUsuario("u.data")
+conj = example()
+print(conj)
+
+
+print (neighborhood(conj[0],conj,0.6))
