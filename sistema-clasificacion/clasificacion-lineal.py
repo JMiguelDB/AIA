1d9b7a5de2a099e56a58745028a5ef36395b18ff
JMiguelDB <jose.miguel.db.17894@gmail.com>
2017-03-06 20:29:36 +0100

    nueva version

--- sistema-clasificacion/clasificacion-lineal.py
+++ sistema-clasificacion/clasificacion-lineal.py
@@ -12,9 +12,9 @@ with open('No-show-Issue-Comma-300k.csv', 'r+') as f:
     reader = csv.reader(f)
     dataset = list(reader)
 
-#Resultados de las pruebas
+#Lista de validacion de las pruebas
 test = np.array(dataset)[1:,5]
-#Datos de pruebas sin la columna de resultados
+#Lista de pruebas sin la columna de validacion
 dataset = np.array(dataset)[1:]
 dataset = np.delete(dataset,5,1)
 
