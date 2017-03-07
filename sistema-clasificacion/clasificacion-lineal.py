fe58ffc2693acd7348e7ac548f7d2105d6cffd0d
JMiguelDB <jose.miguel.db.17894@gmail.com>
2017-03-07 20:15:31 +0100

    Funciona bien

--- sistema-clasificacion/clasificacion-lineal.py
+++ sistema-clasificacion/clasificacion-lineal.py
@@ -30,29 +30,35 @@ def dayToNumber(day):
 #----------- Cargamos los datos -----------------------------------------
 with open('No-show-Issue-Comma-300k.csv', 'r+') as f:
     reader = csv.reader(f)
-    dataset = list(reader)
+    dataset = np.array(list(reader))
 
 #Conjunto de prueba
-test = np.array(dataset)[1:,5]
+test = dataset[1:,5]
 for i in range(len(test)):
     if test[i] == "No-Show":
         test[i] = 0
     else:
         test[i] = 1
+
 #Conjunto de entrenamiento
-dataset = np.array(dataset)[1:]
+dataset = dataset[1:]
 dataset = np.delete(dataset,5,1)
+dataset = np.delete(dataset,[2,3],1)
 #Convertimos el string del dia a un numero y los dias de espera los convertimos a positivo
 for i in range(len(dataset)):
-    dataset[i][4] = dayToNumber(dataset[i][4])
-    dataset[i][13] = abs(int(dataset[i][13]))
+    dataset[i][2] = dayToNumber(dataset[i][2])
+    dataset[i][11] = abs(int(dataset[i][11]))
+    if dataset[i][1] == "M":
+        dataset[i][1] = 1
+    else:
+        dataset[i][1] = 0
 
+dataset = dataset.astype(float)
 #--- Escogemos las características que utilizaremos en el entrenamiento -------
 
 #Escogemos las caracteristicas: Dia, SMS y dias de espera entre la llamada y el dia de la cita
-x_dataset = dataset[:,[4, 12, 13]]
+x_dataset = dataset[:,[2, 10, 11]]
 
-print(x_dataset[0])
 #----- Generamos el conjunto de prueba y de entrenamiento -----------------------
 X_train, X_test, y_train, y_test = train_test_split(x_dataset,test,test_size = 0.25)
 
@@ -65,13 +71,12 @@ normalizador = StandardScaler().fit(X_test)
 Xn_test = normalizador.transform(X_test)
 
 #------ Creamos el sistema de clasificacion -----------------------
-clasificador = SGDClassifier().fit(Xn_train,y_train)
+clasificador = SGDClassifier().fit(X_train,y_train)
 
-y_test_pred = clasificador.predict(Xn_test)
+y_test_pred = clasificador.predict(X_test)
 
 sklearn.metrics.accuracy_score(y_test,y_test_pred)
 
 print(sklearn.metrics.classification_report(y_test,y_test_pred))
-
 
 
