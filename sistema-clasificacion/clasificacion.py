6ea2a50fa4c6b7d5e06a5b8317ab1d0e9ea39365
Jose Miguel <jose.miguel.db.17894@gmail.com>
2017-03-13 13:50:48 +0100

    Añadido arbol de decision

--- sistema-clasificacion/clasificacion.py
+++ sistema-clasificacion/clasificacion.py
@@ -7,6 +7,7 @@ Created on Mon Mar  6 19:48:11 2017
 
 import csv
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
@@ -45,7 +46,7 @@ class clasificacion():
                 dataset[i][1] = 0
         
         self.x_data = dataset.astype(float)
-        self.y_data = test
+        self.y_data = test.astype(float)
     
 
     def __dayToNumber(self,day):
@@ -73,11 +74,19 @@ class clasificacion():
     
     """
     Metodo que genera el conjunto de prueba y de entrenamiento
-    Recibe como parametro la velocidad de aprendizaje
+    Recibe como parametro la velocidad de aprendizaje y el clasificador
     """
-    def train(self,learning_rate = 0.25):
+    def train(self,clasificador,learning_rate = 0.25):
         X_train, X_test, y_train, y_test = train_test_split(self.x_data,self.y_data,test_size = learning_rate)
+
+        clasificador = clasificador.fit(X_train.astype(float),y_train.astype(float))
+        y_test_pred = clasificador.predict(X_test.astype(float))
      
+        print("\n -------------------------- Resultados clasificacion ----------------- \n")
+        print("Precision del clasificador para el conjunto de pruebas:", sklearn.metrics.accuracy_score(y_test.astype(float),y_test_pred.astype(float)))
+        print("Tasa de aciertos/fallos: \n",sklearn.metrics.confusion_matrix(y_test.astype(float),y_test_pred.astype(float)))
+        print("Resumen: \n",sklearn.metrics.classification_report(y_test.astype(float),y_test_pred.astype(float)))
+        #print("Probabilidades de clasificacion", clasificador.predict_proba(X_test.astype(float)))
     """
     Metodo que aplica la validacion cruzada al tipo de clasificador pasado por parametros
     y diviendo el conjunto de datos en k trozos
@@ -88,13 +97,21 @@ class clasificacion():
         #Genera conjuntos de datos de tamaño k
         kfold = KFold(self.x_data.shape[0], k, shuffle = True)
         #Se realiza la valoración cruzada sobre el conjunto de datos
-        valores = cross_val_score(modelo, self.x_data, self.y_data, cv=kfold)
+        valores = cross_val_score(modelo, self.x_data.astype(float), self.y_data.astype(float), cv=kfold)
         #Se predicen los valores a partir del conjunto de prueba
-        predicted = cross_val_predict(modelo, self.x_data, self.y_data, cv=kfold)
+        predicted = cross_val_predict(modelo, self.x_data.astype(float), self.y_data.astype(float), cv=kfold)
         
+        print("\n -------------------------- Resultados validacion cruzada ----------------- \n")
         print("Valoraciones obtenidas en el clasificador:", valores)
         print("Media de las valoraciones:", np.mean(valores))
         print("Error estandar:", scipy.stats.sem(valores))
-        print("Precision del clasificador para el conjunto de pruebas:", sklearn.metrics.accuracy_score(self.y_data,predicted))
-        print("Tasa de aciertos/fallos:",sklearn.metrics.confusion_matrix(self.y_data,predicted))
-        print("Resumen:",sklearn.metrics.classification_report(self.y_data,predicted))
+        print("Precision del clasificador para el conjunto de pruebas:", sklearn.metrics.accuracy_score(self.y_data.astype(float),predicted.astype(float)))
+        print("Tasa de aciertos/fallos: \n",sklearn.metrics.confusion_matrix(self.y_data.astype(float),predicted.astype(float)))
+        print("Resumen: \n",sklearn.metrics.classification_report(self.y_data.astype(float),predicted.astype(float)))
+        
+    def representacion_grafica(self,x,y,x_name,y_name):
+        for tipo,marca,color in zip(range(2),"soD","rgb"):
+            plt.scatter(self.x_data[:,x][self.y_data == tipo],self.x_data[:,y][self.y_data == tipo],marker=marca,c=color)
+        plt.xlabel(x_name)
+        plt.ylabel(y_name)
+        plt.show()
