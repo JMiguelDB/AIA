ba5c84ed75d910fe9b943076e48f4a6527702478
JMiguelDB <jose.miguel.db.17894@gmail.com>
2017-03-27 19:45:00 +0200

    Terminada tarea 4

--- clasificacion-textos/practica_05_NB.py
+++ clasificacion-textos/practica_05_NB.py
@@ -3,7 +3,7 @@
 # =============================
 import votes as votos
 import math
-
+import copy
 # Este trabajo está inspirado en el proyecto "Classification" de The Pacman
 # Projects, desarrollados para el curso de Introducción a la Inteligencia
 # Artificial de la Universidad de Berkeley.
@@ -173,7 +173,19 @@ class ClasificadorNaiveBayes(MetodoClasificacion):
 
         # *********** COMPLETA EL CÓDIGO **************
         self.k = k
+        self.tabla_prob = None
         super().__init__(atributo_clasificacion,clases,atributos,valores_atributos)
+        
+    #Aplicamos el suavizado de Laplace y la probabilidad resultante se almacena en forma logaritmica
+    def suavizadoLaplace(self,tabla_recuento,prob_priori,k):
+        for clas,caracteristicas in tabla_recuento.items():
+            for valores in caracteristicas:
+                for valor in valores:
+                    prob = (valores[valor] + self.k) / (prob_priori[clas] + (k * len(valores)))
+                    #print("valor",prob)
+                    #print("prob",math.log(prob))
+                    valores[valor] = math.log(prob)
+        return tabla_recuento
         
     def entrena(self,entr,clas_entr,valid,clas_valid,autoajuste=True):
 
@@ -214,10 +226,6 @@ class ClasificadorNaiveBayes(MetodoClasificacion):
             prob_priori[tipo] = (count/len(clas_entr))
         self.prob_priori = prob_priori    
         print("Probabilidades a priori: {} \n".format(prob_priori))
-        
-        #Ajustamos el valor de K
-        if autoajuste == True:
-            self.k = 1
           
         #Creamos un diccionario por cada posible clasificacion que contiene una lista de los atributos con sus posibles valores
         for clase_clasif in self.clases:
@@ -234,19 +242,32 @@ class ClasificadorNaiveBayes(MetodoClasificacion):
                 if entr[i][j] in tabla_recuento[clas_entr[i]][j]:
                     tabla_recuento[clas_entr[i]][j][entr[i][j]] += 1 
         
-        print(tabla_recuento, "\n")
+        #print(tabla_recuento, "\n")
         
+        #Ajustamos el valor de K
+        if autoajuste == True:
+            precision = 0
+            print("\n Calculando autoajuste de K \n")
+            for candidato in candidatos:
+                print("\n K:", candidato)
+                tabla = copy.deepcopy(tabla_recuento)
+                prob_result = ClasificadorNaiveBayes.suavizadoLaplace(self,tabla,prob_priori,candidato)
+                self.tabla_prob = copy.deepcopy(prob_result)
+                prec = ClasificadorNaiveBayes.clasifica(self,valid,clas_valid)
+                if prec > precision:
+                    print("Valor k asignado:", candidato)
+                    precision = prec
+                    self.k = candidato
+                
+        print("Valor final de K:", self.k)   
         #Aplicamos el suavizado de Laplace y la probabilidad resultante se almacena en forma logaritmica
         for clas,caracteristicas in tabla_recuento.items():
             for valores in caracteristicas:
                 for valor in valores:
-                    #print(valores[valor], len(valores), sum(valores.values()))
-                    #print(clas, prob_priori[clas])
                     #prob = (valores[valor] + self.k) / (sum(valores.values()) + (self.k * len(valores)))
                     prob = (valores[valor] + self.k) / (prob_priori[clas] + (self.k * len(valores)))
-                    valores[valor] = math.log(prob)
-
-        print("Tabla con las probabilidades de cada caracteristica: \n",tabla_recuento, "\n")
+                    valores[valor] = math.log(prob)    
+        #print("Tabla con las probabilidades de cada caracteristica: \n",tabla_recuento, "\n")
         self.tabla_prob = tabla_recuento
         
     def clasifica(self,test,clas_test):
@@ -265,44 +286,48 @@ class ClasificadorNaiveBayes(MetodoClasificacion):
         """
 
         # *********** COMPLETA EL CÓDIGO **************
-        clasificaciones = []
-        for i in range(len(test)):
-            v = 0
-            clasificacion = ""
-            #Accedemos a la tabla de probabilidades de cada caracteristica
-            for clas,caracteristicas in self.tabla_prob.items():
-                #print(clas,caracteristicas)
-                operation = 0
-                #Comprobamos si el valor de la caracteristica existe y extraemos la probabilidad
-                for j in range(len(test[i])):
-                    #print(test[i][j], caracteristicas[j])
-                    if test[i][j] in caracteristicas[j]:
-                        #tabla_recuento[clas_entr[i]][j][entr[i][j]] += 1 
-                        operation += caracteristicas[j][test[i][j]]
-                sol = math.log(self.prob_priori[clas]) + operation
-                if sol > v:
-                    v = sol
-                    clasificacion = clas
-            clasificaciones.append(clasificacion)
-        print("Clasificaciones obtenidas: \n", clasificaciones)
-        
-        #Calculamos la tasa de aciertos de la clasificacion
-        aciertos = 0
-        for clas in range(len(clasificaciones)):
-            if clasificaciones[clas] == clas_test[clas]:
-                aciertos += 1
-        aciertos = aciertos / len(clasificaciones)
-        
-        print("\n Tasa de aciertos: {}%".format(100*aciertos))
+        if self.tabla_prob == None:
+            raise ClasificadorNoEntrenado("Clasificador no entrenado")
+        else:
+            clasificaciones = []
+            for i in range(len(test)):
+                v = 0
+                clasificacion = ""
+                #Accedemos a la tabla de probabilidades de cada caracteristica
+                for clas,caracteristicas in self.tabla_prob.items():
+                    #print(clas,caracteristicas)
+                    operation = 0
+                    #Comprobamos si el valor de la caracteristica existe y extraemos la probabilidad
+                    for j in range(len(test[i])):
+                        #print(test[i][j], caracteristicas[j])
+                        if test[i][j] in caracteristicas[j]:
+                            #tabla_recuento[clas_entr[i]][j][entr[i][j]] += 1 
+                            operation += caracteristicas[j][test[i][j]]
+                    sol = math.log(self.prob_priori[clas]) + operation
+                    if sol > v:
+                        v = sol
+                        clasificacion = clas
+                clasificaciones.append(clasificacion)
+            #print("Clasificaciones obtenidas: \n", clasificaciones)
+            
+            #Calculamos la tasa de aciertos de la clasificacion
+            aciertos = 0
+            for clas in range(len(clasificaciones)):
+                if clasificaciones[clas] == clas_test[clas]:
+                    aciertos += 1
+            aciertos = aciertos / len(clasificaciones)
+            
+            print("\n Tasa de aciertos: {}%".format(100*aciertos))
+            return aciertos
 # ---------------------------------------------------------------------------
 print("--------------- Ejemplo clasificacion votos de partidos -------------------- \n")
 clasificador_votos = ClasificadorNaiveBayes(votos.votos_atributo_clasificacion,votos.votos_clases,
-                                            votos.votos_atributos,votos.votos_valores_atributos)      
+                                            votos.votos_atributos,votos.votos_valores_atributos,0.001)      
 
 print("Nombre de la clase de clasificacion: {} \n".format(clasificador_votos.atributo_clasificacion))
 print("Posibles valores de clasificacion: {} \n".format(clasificador_votos.clases))
 
-clasificador_votos.entrena(votos.votos_entr,votos.votos_entr_clas,votos.votos_valid,votos.votos_valid_clas,False)
+clasificador_votos.entrena(votos.votos_entr,votos.votos_entr_clas,votos.votos_valid,votos.votos_valid_clas,True)
 clasificador_votos.clasifica(votos.votos_test,votos.votos_test_clas)
 
 print("--------------- Ejemplo clasificacion de digitos -------------------- \n")
@@ -327,7 +352,14 @@ def leeCaracteristicas(pathToFile):
             contador += 1
     return lista
 
-train_data = leeCaracteristicas("digitdata/trainingimages")
+def leeClasificaciones(pathToFile):
+    lista = []
+    with open(pathToFile) as raw_data:
+        for item in raw_data:
+            lista.append(item.split("\n")[0])
+    return lista
+            
+
 
 digitos_atributos = []
 digitos_valor_atributos = {}
@@ -338,8 +370,16 @@ for i in range(28*28):
 clasificador_digitos = ClasificadorNaiveBayes('digito',
                                               ['0','1','2','3','4','5','6','7','8','9'],
                                               digitos_atributos,
-                                              digitos_valor_atributos)
+                                              digitos_valor_atributos,
+                                              0.001)
 
 
+train_data = leeCaracteristicas("digitdata/trainingimages")
+train_clas = leeClasificaciones("digitdata/traininglabels")
+val_data = leeCaracteristicas("digitdata/validationimages")
+val_clas = leeClasificaciones("digitdata/validationlabels")
+test_data = leeCaracteristicas("digitdata/testimages")
+test_clas = leeClasificaciones("digitdata/testlabels")
 
-
+clasificador_digitos.entrena(train_data,train_clas,val_data,val_clas,True)
+clasificador_digitos.clasifica(test_data,test_clas)
